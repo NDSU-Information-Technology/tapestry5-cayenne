95efd3e923c0edf1d44a49f3bba90a9a064ca49d
Richard Frovarp <rfrovarp@gmail.com>
2016-10-22 16:32:15 -0500

    A license and format

--- tapestry5-cayenne-client/src/main/java/com/googlecode/tapestry5cayenne/services/CayenneContextProviderImpl.java
+++ tapestry5-cayenne-client/src/main/java/com/googlecode/tapestry5cayenne/services/CayenneContextProviderImpl.java
@@ -1,3 +1,15 @@
+/*  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.googlecode.tapestry5cayenne.services;
 
 import org.apache.cayenne.CayenneContext;
@@ -17,63 +29,43 @@ import org.apache.tapestry5.services.ApplicationStateManager;
  * @author Kevin Menard
  * @version 1.0
  */
-public class CayenneContextProviderImpl implements ObjectContextProvider
-{
-    private final ApplicationStateManager asm;
-    private final String webServiceUrl;
-    private final String username;
-    private final String password;
-    private final String sharedSessionName;
+public class CayenneContextProviderImpl implements ObjectContextProvider {
+  private final ApplicationStateManager asm;
+  private final String webServiceUrl;
+  private final String username;
+  private final String password;
+  private final String sharedSessionName;
 
-    @SuppressWarnings("unchecked")
-    public CayenneContextProviderImpl(
-            final ApplicationStateManager asm,
+  public CayenneContextProviderImpl(final ApplicationStateManager asm,
+      @Inject @Symbol(TapestryCayenneModule.WEB_SERVICE_URL) final String webServiceUrl,
+      @Inject @Symbol(TapestryCayenneModule.USERNAME) final String username,
+      @Inject @Symbol(TapestryCayenneModule.PASSWORD) final String password,
+      @Inject @Symbol(TapestryCayenneModule.SHARED_SESSION_NAME) final String sharedSessionName) {
+    this.asm = asm;
 
-            @Inject
-            @Symbol(TapestryCayenneModule.WEB_SERVICE_URL)
-            final String webServiceUrl,
+    this.webServiceUrl = webServiceUrl;
+    this.username = username;
+    this.password = password;
+    this.sharedSessionName = sharedSessionName;
+  }
 
-            @Inject
-            @Symbol(TapestryCayenneModule.USERNAME)
-            final String username,
-
-            @Inject
-            @Symbol(TapestryCayenneModule.PASSWORD)
-            final String password,
-
-            @Inject
-            @Symbol(TapestryCayenneModule.SHARED_SESSION_NAME)
-            final String sharedSessionName)
-    {
-        this.asm = asm;
-
-        this.webServiceUrl = webServiceUrl;
-        this.username = username;
-        this.password = password;
-        this.sharedSessionName = sharedSessionName;
+  public ObjectContext currentContext() {
+    try {
+      return BaseContext.getThreadObjectContext();
+    } catch (final IllegalStateException exception) {
+      // make our behavior consistent with DataContextProviderImpl in the server
+      // package.
+      if (asm.exists(ObjectContext.class)) {
+        return asm.get(ObjectContext.class);
+      }
+      return null;
     }
+  }
 
-    public ObjectContext currentContext()
-    {
-        try
-        {
-            return BaseContext.getThreadObjectContext();
-        }
-        catch (final IllegalStateException exception)
-        {
-            //make our behavior consistent with DataContextProviderImpl in the server package.
-            if (asm.exists(ObjectContext.class)) {
-                return asm.get(ObjectContext.class);
-            }
-            return null;
-        }
-    }
+  public ObjectContext newContext() {
+    final ClientConnection conn = new HessianConnection(webServiceUrl, username, password, sharedSessionName);
+    final DataChannel channel = new ClientChannel(conn);
 
-    public ObjectContext newContext()
-    {
-        final ClientConnection conn = new HessianConnection(webServiceUrl, username, password, sharedSessionName);
-        final DataChannel channel = new ClientChannel(conn);
-
-        return new CayenneContext(channel);
-    }
+    return new CayenneContext(channel);
+  }
 }
--- tapestry5-cayenne-client/src/main/java/com/googlecode/tapestry5cayenne/services/TapestryCayenneModule.java
+++ tapestry5-cayenne-client/src/main/java/com/googlecode/tapestry5cayenne/services/TapestryCayenneModule.java
@@ -1,7 +1,14 @@
-/*
- * Created on Mar 18, 2008
- * 
- * 
+/*  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package com.googlecode.tapestry5cayenne.services;
 
@@ -11,40 +18,39 @@ import org.apache.tapestry5.ioc.annotations.SubModule;
 import com.googlecode.tapestry5cayenne.annotations.Cayenne;
 
 @SubModule(TapestryCayenneCoreModule.class)
-public class TapestryCayenneModule
-{
-    /**
-     * Configuration key for setting the URL for the Cayenne Web Service that the client should connect to.
-     */
-    public static final String WEB_SERVICE_URL = "tapestrycayenne.client.web_service_url";
+public class TapestryCayenneModule {
+  /**
+   * Configuration key for setting the URL for the Cayenne Web Service that the
+   * client should connect to.
+   */
+  public static final String WEB_SERVICE_URL = "tapestrycayenne.client.web_service_url";
 
-    /**
-     * Configuration key for setting the username to use in basic auth to the Cayenne Web Service.
-     */
-    public static final String USERNAME = "tapestrycayenne.client.username";
+  /**
+   * Configuration key for setting the username to use in basic auth to the
+   * Cayenne Web Service.
+   */
+  public static final String USERNAME = "tapestrycayenne.client.username";
 
-    /**
-     * Configuration key for setting the password to use in basic auth ot the Cayenne Web Service.
-     */
-    public static final String PASSWORD = "tapestrycayenne.client.password";
+  /**
+   * Configuration key for setting the password to use in basic auth ot the
+   * Cayenne Web Service.
+   */
+  public static final String PASSWORD = "tapestrycayenne.client.password";
 
-    /**
-     * Configuration key for setting the Hessian shared session name.
-     */
-    public static final String SHARED_SESSION_NAME = "tapestrycayenne.client.shared_session_name";
+  /**
+   * Configuration key for setting the Hessian shared session name.
+   */
+  public static final String SHARED_SESSION_NAME = "tapestrycayenne.client.shared_session_name";
 
-    public static void contributeFactoryDefaults(final MappedConfiguration<String,String> conf)
-    {
-        conf.add(WEB_SERVICE_URL, "http://localhost:8080/cws");
-        conf.add(USERNAME, "");
-        conf.add(PASSWORD, "");
-        conf.add(SHARED_SESSION_NAME, "");
-    }
+  public static void contributeFactoryDefaults(final MappedConfiguration<String, String> conf) {
+    conf.add(WEB_SERVICE_URL, "http://localhost:8080/cws");
+    conf.add(USERNAME, "");
+    conf.add(PASSWORD, "");
+    conf.add(SHARED_SESSION_NAME, "");
+  }
 
-    @SuppressWarnings("unchecked")
-    public static void bind(final ServiceBinder binder)
-    {
-        binder.bind(ObjectContextProvider.class, CayenneContextProviderImpl.class)
-            .withMarker(Cayenne.class).withId("CayenneContext");
-    }
+  @SuppressWarnings("unchecked")
+  public static void bind(final ServiceBinder binder) {
+    binder.bind(ObjectContextProvider.class, CayenneContextProviderImpl.class).withMarker(Cayenne.class).withId("CayenneContext");
+  }
 }
